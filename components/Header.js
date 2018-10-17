b79734a46ea14c5088f70008744a5d119d66c217
Martin M <mxa.code@gmail.com>
2018-10-17 22:33:28 +0100

    feat: add nprogress to visualise route changes

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import styled from 'styled-components';
 
 import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  Router.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
