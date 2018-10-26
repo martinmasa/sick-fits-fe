2138ceeecb14c00d474a3c36902a300b970bf51f
Martin M <mxa.code@gmail.com>
2018-10-27 00:14:00 +0300

    feat: display list of items from database

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Items from '../components/Items';
 
 const Home = (props) => {
   return (
     <div>
       <p>Home</p>
+      <Items />
     </div>
   );
 };
