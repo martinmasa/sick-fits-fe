a629d8abbe168fa36c541300d24d58e153b0f979
Martin M <mxa.code@gmail.com>
2018-12-04 23:42:11 +0000

    feat: add delete item logic

--- components/Item.js
+++ components/Item.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
+import DeleteItem from './DeleteItem';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
@@ -45,7 +46,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete item</DeleteItem>
         </div>
       </ItemStyles>
     );
